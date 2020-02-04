37522029f15fcaf6cb9afe3a72f0410d797726cc
Adrien <adrien.masson@hotmail.fr>
2020-02-04 14:51:02 +0100

    List

--- app/src/main/java/com/maxadri/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/maxadri/tasklist/TaskListFragment.kt
@@ -4,16 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.*
 import com.maxadri.todo.R
 
 class TaskListFragment : Fragment() {
+
+    private val taskList = listOf("Task 1", "Task 2", "Task 3")
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater.inflate(R.layout.fragment_task_list, container, false), container, savedInstanceState)
+        inflater.inflate(R.layout.fragment_task_list, container, false)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        LayoutManager
+        Adapter
+    }
+}
+
+class TaskListAdapter(private val taskList: List<String>) : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>() {
+
+    inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(taskTitle: String) {
+            // C'est ici qu'on reliera les données et les listeners une fois l'adapteur implémenté
+        }
     }
 }
